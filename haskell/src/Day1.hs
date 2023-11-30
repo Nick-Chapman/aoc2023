bc24c1f42c5ff30cee161dedf040db8661ba859e
Nick Chapman <nic@amadido.co.uk>
2023-11-30 15:12:30 +0000

    prep for this years day 1

--- haskell/src/Day1.hs
+++ haskell/src/Day1.hs
@@ -1,15 +1,15 @@
 module Day1 (main) where
 
 import Misc (check)
-import Data.List as List (sort)
+--import Data.List as List (sort)
 import Par4 (Par,parse,many,separated,nl,int)
 
 main :: IO ()
 main = do
   s <- readFile "../input/day1.input"
   let inp = parse gram s
-  print ("day1, part1", check 71506 $ part1 inp)
-  print ("day1, part2", check 209603 $ part2 inp)
+  print ("day1, part1", check 0 $ part1 inp)
+  print ("day1, part2", check 0 $ part2 inp)
 
 type Setup = [[Int]]
 
@@ -20,7 +20,7 @@ gram = separated nl chunk
     num = do x <- int; nl; pure x
 
 part1 :: Setup -> Int
-part1 = maximum . map sum
+part1 = undefined
 
 part2 :: Setup -> Int
-part2 = sum . take 3 . reverse . List.sort . map sum
+part2 = undefined
