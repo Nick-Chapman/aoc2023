85c7cc2297caa8545b2c9b5fa950d3f47ef35b3d
Nick Chapman <nic@amadido.co.uk>
2024-02-01 11:43:12 +0000

    day23, part2, WIP

--- haskell/src/Day23.hs
+++ haskell/src/Day23.hs
@@ -31,8 +31,89 @@ main = do
   print ("day23, part1 (sam)", check 94 $ part1 _sam)
   print ("day23, part1", check 2018 $ part1 _inp)
 
+  s2 <- part2 _sam
+  print ("day23, part2 (sam)", check 154 $ s2)
+
+  i2 <- part2 _inp
+  print ("day23, part2", check 0 $ i2)
+
 type Pos = (Int,Int)
-data Dir = L | R | U | D deriving (Eq,Show)
+data Dir = L | R | U | D deriving (Eq,Ord,Show)
+
+type M = Map (Set Pos,(Dir,Pos)) (Maybe Int)
+
+part2 :: Maze -> IO Int
+part2 maze = do
+  let h = length maze
+  let w = length (head maze)
+  let stop p = p == (w-1,h)
+  let
+    notSlippy :: Cell -> Cell
+    notSlippy = \case Hash -> Hash; _ -> Dot
+  let
+    at :: Pos -> Maybe Cell
+    at = flip Map.lookup m
+      where m = Map.fromList
+              [ ((x,y), notSlippy cell)
+              | (y,line) <- zip [1..] maze , (x,cell) <- zip [1..] line
+              ]
+  let
+    walk :: M -> Set Pos -> Int -> (Dir,Pos) -> IO (M, Maybe Int)
+    walk m vs q dp = do
+      --print (Map.size m, "walk",q,dp)
+      case Map.lookup (vs,dp) m of
+        Just res -> do
+          print (Map.size m, "walk",q,dp)
+          print "HIT"
+          pure (m,res)
+        Nothing -> do
+          (m',res) <- walkF m vs q dp
+          let m'' = Map.insert (vs,dp) res m'
+          pure (m'',res)
+
+    walkF :: M -> Set Pos -> Int -> (Dir,Pos) -> IO (M, Maybe Int)
+    walkF m vs q dp = do
+      --print ("walk",q,dp)
+      --print ("walk",q,dp,"#",Set.toList vs)
+      case walkToJunction stop at dp of
+        Nothing -> do
+          error "never here why?"
+          --print ("walk",q,dp,"-->","DEAD-END/LOOP")
+          --pure Nothing
+        Just (i,opt) -> do
+          case opt of
+            Nothing -> do
+              --print ("walk",q,dp,"-->","FINISHED")
+              pure (m, Just i)
+            Just (jpos,dps) -> do
+              if jpos `Set.member` vs then pure (m,Nothing) else do
+                let vs' = Set.insert jpos vs
+                --print ("walk",q,dp,"-->",dps)
+                let q' = q+1
+                let
+                  walks :: M -> [Int] -> [(Dir,Pos)] -> IO (M,[Int])
+                  walks m acc = \case
+                    [] -> pure (m,acc)
+                    x:xs -> do
+                      (m',opt) <- walk m vs' q' x
+                      let acc' = (case opt of Nothing -> acc; Just n -> n:acc)
+                      walks m' acc' xs
+
+                --ns' <- mapM (walk vs' (q+1)) dps
+                --let ns :: [Int] = catMaybes ns'
+                (m,ns) <- walks m [] dps
+                case ns of
+                  [] -> pure (m,Nothing)
+                  _ -> pure (m,Just (i + maximum ns))
+
+  let start :: Pos = (2,1)
+  let m0 = Map.empty
+  walk m0 (Set.singleton (2,0)) 0 (D,start) >>= \case
+    (_,Nothing) -> error "no solution"
+    (_,Just res) -> pure res
+
+
+
 
 part1 :: Maze -> Int
 part1 maze = do
@@ -40,37 +121,39 @@ part1 maze = do
   let w = length (head maze)
   let stop p = p == (w-1,h)
   let
-    at :: Pos -> Cell
-    at = flip look m
+    at :: Pos -> Maybe Cell
+    at = flip Map.lookup m
       where m = Map.fromList
               [ ((x,y),cell) | (y,line) <- zip [1..] maze , (x,cell) <- zip [1..] line ]
   let
     walk :: (Dir,Pos) -> Int
     walk dp = do
-      let (i,opt) = walkToJunction stop at dp
-      case opt of
-        Nothing -> i
-        Just dps -> i + maximum (map walk dps)
+      case walkToJunction stop at dp of
+        Nothing -> undefined
+        Just (i,opt) -> do
+          case opt of
+            Nothing -> i
+            Just (_,dps) -> i + maximum (map walk dps)
   let start :: Pos = (2,1)
   walk (D,start)
 
-walkToJunction :: (Pos -> Bool) -> (Pos -> Cell) -> (Dir,Pos) -> (Int,Maybe [(Dir,Pos)])
+walkToJunction :: (Pos -> Bool) -> (Pos -> Maybe Cell) -> (Dir,Pos) -> Maybe (Int,Maybe (Pos,[(Dir,Pos)]))
 walkToJunction stop at = loop 0
   where
-    loop i (dirLast,pos) = if stop pos then (i,Nothing) else do
+    loop i (dirLast,pos) = if stop pos then Just (i,Nothing) else do
       let
         alts :: [(Dir,Pos)]
         alts =
           [ (dir,pos') | dir <- allWays
                        , dir /= opp dirLast
                        , let pos' = adj pos dir
-                       , let cell = at pos'
+                       , cell <- maybeToList (at pos')
                        , canStep cell dir
                        ]
       case alts of
-        [] -> error (show ("no alts",dirLast,pos))
+        [] -> Nothing --error (show ("no alts",dirLast,pos))
         [dp] -> loop (i+1) dp
-        _ -> (i+1,Just alts)
+        _ -> Just (i+1,Just (pos,alts))
 
 allWays :: [Dir]
 allWays = [L,R,U,D]
